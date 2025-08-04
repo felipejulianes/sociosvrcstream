bfd0521265ac636f278eb27b6fbcb2f311c493af
felipejulianes <136484753+felipejulianes@users.noreply.github.com>
2025-08-04 17:19:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -48,12 +48,16 @@ st.write(data["mensaje"])
 st.markdown("Hacé posible que un joven siga disfrutando del rugby. Con tu aporte ayudás a cubrir parte de la cuota de quien hoy no puede afrontarla y promovés su formación deportiva y personal.")
 st.markdown("## Elegí un monto para donar")
 
-col1, col2 = st.columns(2)
+col1, col2, col3, col4 = st.columns(4)
+
 with col1:
     st.link_button("$20K", data["links"]["20000"])
-    st.link_button("$60.000", data["links"]["60000"])
 with col2:
     st.link_button("$40.000", data["links"]["40000"])
+with col3:
+    st.link_button("$60.000", data["links"]["60000"])
+with col4:
     st.link_button("Monto libre", data["links"]["libre"])
+
 
 st.caption("Gracias por confiar en lo que hacemos ❤️")
